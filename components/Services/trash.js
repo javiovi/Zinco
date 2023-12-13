99655beb6f6376c52c2b3402fd176037245ddb2f
JaviOvi <106032666+javiovi@users.noreply.github.com>
2023-12-13 18:05:45 -0300

    Update trash.js

--- components/Services/trash.js
+++ components/Services/trash.js
@@ -1,3 +1,1 @@
-<Link href='/blog' onClick={() => setOpen(!open)} className={`flex ${pathname=='/blog'?'text-[#414141]':'text-[#8D8D8D]'}  gap-8 cursor-pointer`}> <p>04</p>
-<p>TENDENCIAS</p>
-</Link> 
+
