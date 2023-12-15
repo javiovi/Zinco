f5c8e1013b0b1110990b495b42b4d18f0a0e66ba
Javiov <javieroviedo0512@gmail.com>
2023-12-15 12:03:19 -0300

    15/12

--- components/About/Trajectory.js
+++ components/About/Trajectory.js
@@ -21,9 +21,13 @@ export default function Trajectory() {
             </h2>
             {/*  a lot of repetition but should prioritize fixing it later */}
             <div className='mx-auto max-w-[1262px]'>
+            <div data-aos="fade-up" className='flex border-b border-[#493BBB] gap-14 sm:gap-24 md:gap-48 pt-10 pb-6 items-start min-h-[130px] md:min-h-[167px]'>
+                    <p className='font-black font-archivo text-lg sm:text-[22px]'>2024</p>
+                    <p className='font-rubik font-normal text-sm sm:text-base md:text-xl'>Seguimos creciendo, desarrollando ingenierías para proyectos residenciales y comerciales.</p>
+                </div>
                 <div data-aos="fade-up" className='flex border-b border-[#493BBB] gap-14 sm:gap-24 md:gap-48 pt-10 pb-6 items-start min-h-[130px] md:min-h-[167px]'>
                     <p className='font-black font-archivo text-lg sm:text-[22px]'>2023</p>
-                    <p className='font-rubik font-normal text-sm sm:text-base md:text-xl'></p>
+                    <p className='font-rubik font-normal text-sm sm:text-base md:text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat ipsum ac eros lacinia suscipit. Sed sed tortor eu eros.</p>
                 </div>
                 <div data-aos="fade-up" className='flex border-b border-[#493BBB] gap-14 sm:gap-24  md:gap-48 pt-10 pb-6 items-start min-h-[130px] md:min-h-[167px]'>
                     <p className='font-black font-archivo text-lg sm:text-[22px]'>2022</p>
